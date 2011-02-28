daac0c1402447883d68a02187e365741ca2305de
daniele <d.milan@hackingteam.it>
2011-02-28 11:53:17 +0100

    Updated git ignores. Stubbed trace configuration. Stubbed applications for db and worker.

--- lib/rcs-db/db.rb
+++ lib/rcs-db/db.rb
@@ -8,19 +8,47 @@ require 'rcs-common/trace'
 module RCS
 module DB
 
-class Application
-  # To change this template use File | Settings | File Templates.
-  def run(options)
-    puts "DB up and running!"
-    return 0
+  class Application
+    include RCS::Tracer
+
+    # To change this template use File | Settings | File Templates.
+    def run(options)
+
+      # if we can't find the trace config file, default to the system one
+      if File.exist? 'trace.yaml' then
+        typ = Dir.pwd
+        ty = 'trace.yaml'
+      else
+        typ = File.dirname(File.dirname(File.dirname(__FILE__)))
+        ty = typ + "/config/trace.yaml"
+        #puts "Cannot find 'trace.yaml' using the default one (#{ty})"
+      end
+
+      # ensure the log directory is present
+      Dir::mkdir(Dir.pwd + '/log') if not File.directory?(Dir.pwd + '/log')
+
+      # initialize the tracing facility
+      begin
+        trace_init typ, ty
+      rescue Exception => e
+        puts e
+        exit
+      end
+
+      begin
+        version = File.read(Dir.pwd + '/config/version.txt')
+        trace :info, "Starting the RCS Database #{version}..."
+      end
+      
+      return 0
+    end
+
+    # we instantiate here an object and run it
+    def self.run!(*argv)
+      return Application.new.run(argv)
+    end
   end
 
- # we instantiate here an object and run it
-  def self.run!(*argv)
-    return Application.new.run(argv)
-  end
-end
 
-
-end
-end
+end # DB::
+end # RCS::
--- lib/rcs-worker/worker.rb
+++ lib/rcs-worker/worker.rb
@@ -9,17 +9,46 @@ module RCS
 module Worker
 
 class Application
+  include RCS::Tracer
+  
   # To change this template use File | Settings | File Templates.
   def run(options)
-    puts "Worker up and running!"
+    
+    # if we can't find the trace config file, default to the system one
+    if File.exist? 'trace.yaml' then
+      typ = Dir.pwd
+      ty = 'trace.yaml'
+    else
+      typ = File.dirname(File.dirname(File.dirname(__FILE__)))
+      ty = typ + "/config/trace.yaml"
+      #puts "Cannot find 'trace.yaml' using the default one (#{ty})"
+    end
+
+    # ensure the log directory is present
+    Dir::mkdir(Dir.pwd + '/log') if not File.directory?(Dir.pwd + '/log')
+
+    # initialize the tracing facility
+    begin
+      trace_init typ, ty
+    rescue Exception => e
+      puts e
+      exit
+    end
+
+    begin
+      version = File.read(Dir.pwd + '/config/version.txt')
+      trace :info, "Starting a RCS Worker #{version}..."
+    end
+
+
     return 0
   end
-
- # we instantiate here an object and run it
+  
+  # we instantiate here an object and run it
   def self.run!(*argv)
     return Application.new.run(argv)
   end
 end
 
-end
-end
+end # Worker::
+end # RCS::
