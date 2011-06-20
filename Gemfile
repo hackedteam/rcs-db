dddead7a823c9e0b409bb948460e08f08ba1d629
daniele <d.milan@hackingteam.it>
2011-06-20 17:17:28 +0200

    Added support for SimpleCov code coverage.

--- Gemfile
+++ Gemfile
@@ -28,8 +28,8 @@ gem 'mail'
 group :development do
   gem "bundler", "~> 1.0.0"
   gem "jeweler", "~> 1.5.2"
-  gem "rcov", ">= 0"
   gem 'test-unit'
+  gem 'simplecov'
   
   gem "rcs-common", :path => "../rcs-common"
 end
--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start if ENV['COVERAGE']
+
 require 'rubygems'
 require 'bundler'
 begin
@@ -7,12 +10,13 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
-require 'test/unit'
 
+require 'test/unit'
+require 'minitest/mock'
+
+=begin
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rcs-db'
 require 'rcs-worker'
-
-class Test::Unit::TestCase
-end
+=end
