31ff5edade52c8a2e4ec5fc179685d2d0a2f111a
Daniele Milan <milan.daniele@gmail.com>
2011-06-23 23:46:40 +0200

    Added method to Audit db object for filtered queries.

--- lib/rcs-db/db_objects/audit.rb
+++ lib/rcs-db/db_objects/audit.rb
@@ -26,7 +26,41 @@ class Audit
   index :backdoor
   
   store_in :audit
-  
+
+  def self.filter(filter, start_index = nil, num_items = nil)
+    filter_hash = {}
+
+    # date filters must be treated separately
+    if filter.has_key? 'from' and filter.has_key? 'to'
+      filter_hash[:time.gte] = filter.delete('from')
+      filter_hash[:time.lte] = filter.delete('to')
+      #trace :debug, "Filtering date from #{filter['from']} to #{filter['to']}."
+    end
+
+    # desc filters must be handled as a regexp
+    if filter.has_key? 'desc'
+      #trace :debug, "Filtering description by keywork '#{filter['desc']}'."
+      filter_hash[:desc] = Regexp.new(filter.delete('desc'), true)
+    end
+
+    # copy remaining filtering criteria (if any)
+    filtering = Audit
+    filter.each_key do |k|
+      filtering = filtering.any_in(k.to_sym => filter[k])
+    end
+
+    # paging
+    unless start_index.nil? or num_items.nil?
+      #trace :debug, "Querying with filter #{filter_hash}."
+      query = filtering.where(filter_hash).order_by([[:time, :asc]]).skip(start_index).limit(num_items)
+    else
+      # without paging, return everything
+      query = filtering.where(filter_hash).order_by([[:time, :asc]])
+    end
+
+    return query
+  end
+
   def to_flat_array
     column_names = Audit.fields.keys
     column_names.delete('_type') if fields.has_key? '_type'
