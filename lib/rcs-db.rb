182914b893e5d1c2637a9adeaeab6d72cb89bef0
alor <alberto.ornaghi@gmail.com>
2011-10-19 15:35:36 +0200

    fixed the loading of relative paths

--- lib/rcs-db.rb
+++ lib/rcs-db.rb
@@ -3,11 +3,13 @@ Dir.chdir File.dirname(File.dirname(File.realpath(__FILE__)))
 
 # release file are encrypted and stored in a different directory
 if File.directory?(Dir.pwd + '/lib/rcs-db-release')
-  require_relative 'rcs-db-release/db.rb'
+  require_relative 'rcs-db-release/migration'
+  require_relative 'rcs-db-release/db'
 # otherwise we are using development code
 elsif File.directory?(Dir.pwd + '/lib/rcs-db')
   puts "WARNING: Executing clear text code... (debug only)"
-  require_relative 'rcs-db/db.rb'
+  require_relative 'rcs-db/migration'
+  require_relative 'rcs-db/db'
 else
   puts "FATAL: cannot find any rcs-db code!"
 end
