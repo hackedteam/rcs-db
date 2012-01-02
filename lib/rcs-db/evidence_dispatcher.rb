d3795bafa65b166745f5616c1d11d872b542192c
daniele <d.milan@hackingteam.it>
2012-01-02 17:17:20 +0100

    Evidence dispatcher with round-robin shard selection, still WIP.

--- lib/rcs-db/evidence_dispatcher.rb
+++ lib/rcs-db/evidence_dispatcher.rb
@@ -1,6 +1,8 @@
 # from RCS::Common
 require 'rcs-common/trace'
 
+require_relative 'shard'
+
 module RCS
 module DB
 
@@ -8,9 +10,29 @@ class EvidenceDispatcher
   include Singleton
   include RCS::Tracer
   
+  def initialize
+    @shards = Array.new
+
+    # for each known shard, prepare a list of notification
+    Shard.all['shards'].each_with_index do |shard, index|
+      puts "#{index} #{shard}"
+      ip, port = shard['host'].split(':')
+      puts "#{index} - SHARD #{shard['_id']} IP #{ip}"
+      @shards[index] = {host: ip, queue: []}
+    end
+    # worker polling thread, check for health status
+  end
+  
   def notify(evidence_id, ident, instance)
     agent = ::Item.agents.where({ident: ident, instance: instance}).first
     # spread evidences to shards, each shard getting ALL the evidences for the same agent (round-robin)
+    
+    puts @shards.inspect
+    
+    hash_string = "#{ident}(#{instance})"
+    shard_idx = hash_string.hash % @shards.length
+    puts "EVIDENCE FROM #{ident} (#{instance}) -> SHARD #{@shards[shard_idx][:host]}"
+
     trace :info, "Processing evidence #{evidence_id} for agent #{agent[:name]} "
     return true
   end
