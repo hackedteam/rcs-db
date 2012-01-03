532830fd1b3e70e5dfedd218cf58a6d1ab7e030c
alor <alor@hackingteam.it>
2012-01-03 15:04:51 +0100

    trivial

--- hasp/hasp-key/dllmain.cpp
+++ hasp/hasp-key/dllmain.cpp
@@ -10,13 +10,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH:
-		InitializeCriticalSection(&dec_critic);
-		break;
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
+		case DLL_PROCESS_ATTACH:
+			InitializeCriticalSection(&dec_critic);
+			break;
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
+			break;
 	}
 	return TRUE;
 }
--- hasp/hasp-key/stdafx.h
+++ hasp/hasp-key/stdafx.h
@@ -5,12 +5,9 @@
 
 #pragma once
 
-#include "targetver.h"
+#include <SDKDDKVer.h>
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
 
-
-
-// TODO: reference additional headers your program requires here
