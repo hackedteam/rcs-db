becb7312c3c84c9d9e98e1ab78f98082c921f890
alor <alor@hackingteam.it>
2012-01-27 10:38:57 +0100

    fixes for windows

--- lib/rcs-worker.rb
+++ lib/rcs-worker.rb
@@ -6,7 +6,7 @@ Dir.chdir File.dirname(File.dirname(File.realpath(__FILE__)))
 
 # release file are encrypted and stored in a different directory
 if File.directory?(Dir.pwd + '/lib/rcs-worker-release')
-  require_relative 'rcs-worker-release/db.rb'
+  require_relative 'rcs-worker-release/worker'
 # otherwise we are using development code
 elsif File.directory?(Dir.pwd + '/lib/rcs-worker')
   puts "WARNING: Executing clear text code... (debug only)"
