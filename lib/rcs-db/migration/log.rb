deadeeff77036373cc9d5e2ea56cccdaf29e9682
alor <alberto.ornaghi@gmail.com>
2012-02-02 17:16:36 +0100

    rename "to" -> "rcpt"

--- lib/rcs-db/migration/log.rb
+++ lib/rcs-db/migration/log.rb
@@ -204,7 +204,7 @@ class LogMigration
             conversion = {:latitude => :latitude, :longitude => :longitude, :varchar2 => :type}
         end
       when 'MAIL', 'MMS', 'SMS'
-        conversion = {:varchar1 => :from, :varchar2 => :to, :varchar3 => :subject, :int1 => :size, :int2 => :status, :longtext1 => :content}
+        conversion = {:varchar1 => :from, :varchar2 => :rcpt, :varchar3 => :subject, :int1 => :size, :int2 => :status, :longtext1 => :content}
       when 'MIC'
         conversion = {:int1 => :duration, :int3 => :status}
       when 'MOUSE'
