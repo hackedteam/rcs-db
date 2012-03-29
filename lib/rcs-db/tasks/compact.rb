7f92214cd176ae04b3be593a656251752d13e1cd
alor <alberto.ornaghi@gmail.com>
2012-03-29 15:17:16 +0200

    compact db task

--- lib/rcs-db/tasks/compact.rb
+++ lib/rcs-db/tasks/compact.rb
@@ -8,13 +8,24 @@ class CompactTask
   include RCS::Tracer
 
   def total
-    raise "ciao conrad"
+    collections = Mongoid::Config.master.collection_names.keep_if {|c| c['logs.'].nil? and c['system.'].nil?}
+    collections.size * Shard.count
   end
   
   def next_entry
     yield @description = "Compacting DB"
 
-    
+    Shard.all['shards'].each do |shard|
+      host, port = shard['host'].split(':')
+
+      db = Mongo::Connection.new(host, port.to_i).db("rcs")
+
+      db.collection_names.sort.keep_if {|c| c['logs.'].nil? and c['system.'].nil?}.each do |coll|
+        yield @description = "Compacting #{coll}"
+        db.command({compact: coll})
+      end
+    end
+
     @description = "DB compacted successfully"
   end
 end
