3b018c4272e59e467a042ffabb1bd6e7a0e6919a
alor <alberto.ornaghi@gmail.com>
2012-07-09 15:11:06 +0200

    trivial

--- lib/rcs-worker/instance_worker.rb
+++ lib/rcs-worker/instance_worker.rb
@@ -196,7 +196,7 @@ class InstanceWorker
             retry
           rescue Exception => e
             trace :fatal, "[#{raw_id}:#{@ident}:#{@instance}] Unrecoverable error processing evidence #{raw_id}: #{e.message}"
-            trace :debug, "[#{raw_id}:#{@ident}:#{@instance}] EXCEPTION: " + e.backtrace.join("\n")
+            trace :fatal, "[#{raw_id}:#{@ident}:#{@instance}] EXCEPTION: " + e.backtrace.join("\n")
 
             Dir.mkdir "decoding_failed" unless File.exists? "decoding_failed"
             path = "decoding_failed/#{raw_id}.dec"
