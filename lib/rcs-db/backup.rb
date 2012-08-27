8db5c350f1ac5bd86b1e8791548beef56eb0eb97
alor <alberto.ornaghi@gmail.com>
2012-08-27 14:56:07 +0200

    don't backup cores in metadata

--- lib/rcs-db/backup.rb
+++ lib/rcs-db/backup.rb
@@ -77,7 +77,7 @@ class BackupManager
       case backup.what
         when 'metadata'
           # don't backup evidence collections
-          params[:coll].delete_if {|x| x['evidence.'] || x['grid.']}
+          params[:coll].delete_if {|x| x['evidence.'] || x['grid.'] || x['cores']}
         when 'full'
           # we backup everything... woah !!
         else
