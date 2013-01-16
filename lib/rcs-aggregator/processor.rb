84bed1a817bfcd902a66ea00106264cc39bcecd9
alor <alberto.ornaghi@gmail.com>
2013-01-16 16:23:03 +0100

    support for multiple recipients for messages, calls and chat

--- lib/rcs-aggregator/processor.rb
+++ lib/rcs-aggregator/processor.rb
@@ -41,34 +41,38 @@ class Processor
 
     trace :info, "Processing #{ev.type} for target #{target.name}"
 
+    data = []
+
     # extract peer(s) from call, mail, chat, sms
     data = extract_data(ev) if ['call', 'chat', 'message'].include? ev.type
 
     trace :debug, ev.data.inspect
 
-    # already exist?
-    #   update
-    # else
-    #   create new one
+    data.each do |entry|
+      # already exist?
+      #   update
+      # else
+      #   create new one
 
-    type = ev.type
-    type = data[:type] unless data[:type].nil?
+      type = ev.type
+      type = entry[:type] unless entry[:type].nil?
 
-    # we need to find a document that is in the same day, same type and that have the same peer and versus
-    # if not found, create a new entry, otherwise increment the number of occurrences
-    params = {day: Time.at(ev.da).strftime('%Y%m%d'), type: type, data: {peer: data[:peer], versus: data[:versus]}}
+      # we need to find a document that is in the same day, same type and that have the same peer and versus
+      # if not found, create a new entry, otherwise increment the number of occurrences
+      params = {day: Time.at(ev.da).strftime('%Y%m%d'), type: type, data: {peer: entry[:peer], versus: entry[:versus]}}
 
-    # find the existing aggregate or create a new one
-    agg = Aggregate.collection_class(entry['target_id']).find_or_create_by(params)
+      # find the existing aggregate or create a new one
+      agg = Aggregate.collection_class(entry['target_id']).find_or_create_by(params)
 
-    # we are sure we have the object persisted in the db
-    # so we have to perform an atomic operation because we have multiple aggregator working concurrently
-    agg.inc(:count, 1)
+      # we are sure we have the object persisted in the db
+      # so we have to perform an atomic operation because we have multiple aggregator working concurrently
+      agg.inc(:count, 1)
 
-    # sum up the duration (or size)
-    agg.inc(:size, data[:size])
+      # sum up the duration (or size)
+      agg.inc(:size, entry[:size])
 
-    trace :info, "Aggregated #{target.name}: #{agg.day} #{agg.type} #{agg.count} #{agg.data.inspect} " + (type.eql?('call') ? "#{agg.size} sec" : "#{agg.size.to_s_bytes}")
+      trace :info, "Aggregated #{target.name}: #{agg.day} #{agg.type} #{agg.count} #{agg.data.inspect} " + (type.eql?('call') ? "#{agg.size} sec" : "#{agg.size.to_s_bytes}")
+    end
 
   rescue Exception => e
     trace :error, "Cannot process evidence: #{e.message}"
@@ -76,24 +80,36 @@ class Processor
   end
 
   def self.extract_data(ev)
-    data = {}
+    data = []
 
     case ev.type
       when 'call'
-        data = {:peer => ev.data['peer'], :versus => ev.data['incoming'] == 1 ? :in : :out, :type => ev.data['program'], :size => ev.data['duration'].to_i}
+        # multiple peers creates multiple entries
+        ev.data['peer'].split(',').each do |peer|
+          data << {:peer => peer.strip, :versus => ev.data['incoming'] == 1 ? :in : :out, :type => ev.data['program'], :size => ev.data['duration'].to_i}
+        end
       when 'chat'
         if ev.data['peer']
           # old chat format
-          data = {:peer => ev.data['peer'], :versus => nil, :type => ev.data['program'], :size => ev.data['content'].length}
+          # multiple rcpts creates multiple entries
+          ev.data['peer'].split(',').each do |peer|
+            data << {:peer => peer.strip, :versus => nil, :type => ev.data['program'], :size => ev.data['content'].length}
+          end
         else
           # new chat format
-          data = {:peer => ev.data['incoming'] == 1 ? ev.data['from'] : ev.data['rcpt'], :versus => ev.data['incoming'] == 1 ? :in : :out, :type => ev.data['program'], :size => ev.data['content'].length}
+          # multiple rcpts creates multiple entries
+          ev.data['rcpt'].split(',').each do |rcpt|
+            data << {:peer => ev.data['incoming'] == 1 ? ev.data['from'] : rcpt.strip, :versus => ev.data['incoming'] == 1 ? :in : :out, :type => ev.data['program'], :size => ev.data['content'].length}
+          end
         end
       when 'message'
-        if ev.data['type'] == :mail
-          data = {:peer => ev.data['incoming'] == 1 ? ev.data['from'] : ev.data['rcpt'], :versus => ev.data['incoming'] == 1 ? :in : :out, :type => ev.data['type'], :size => ev.data['body'].length}
-        else
-          data = {:peer => ev.data['incoming'] == 1 ? ev.data['from'] : ev.data['rcpt'], :versus => ev.data['incoming'] == 1 ? :in : :out, :type => ev.data['type'], :size => ev.data['content'].length}
+        # multiple rcpts creates multiple entries
+        ev.data['rcpt'].split(',').each do |rcpt|
+          if ev.data['type'] == :mail
+            data << {:peer => ev.data['incoming'] == 1 ? ev.data['from'] : rcpt.strip, :versus => ev.data['incoming'] == 1 ? :in : :out, :type => ev.data['type'], :size => ev.data['body'].length}
+          else
+            data << {:peer => ev.data['incoming'] == 1 ? ev.data['from'] : rcpt.strip, :versus => ev.data['incoming'] == 1 ? :in : :out, :type => ev.data['type'], :size => ev.data['content'].length}
+          end
         end
     end
 
