ed5bfe35a1e396ba606c700b72b088c96e30892f
alor <alberto.ornaghi@gmail.com>
2013-02-07 15:11:17 +0100

    definitive fix for windows

--- lib/rcs-ocr/tika.rb
+++ lib/rcs-ocr/tika.rb
@@ -16,18 +16,13 @@ class Tika
 
     def transform(input_file, output_file)
 
-      # under windows the text mode (-t) is broken for arabic chars
-      # se we inoke it in html mode and strip the entities
-      text = RCS::DB::CrossPlatform.exec_with_output "java", "-jar ocr/tika/tika.jar #{input_file}"
+      text = RCS::DB::CrossPlatform.exec_with_output "java", "-jar ocr/tika/tika.jar --text --encoding=UTF-8 #{input_file}"
       #meta = RCS::DB::CrossPlatform.exec_with_output "java", "-jar ocr/tika/tika.jar -m #{input_file}"
 
       return false if text.nil? or text.size == 0
 
-      # strip the html entities
-      text_plain = text.strip_html_tags
-
       # write the output
-      File.open(output_file, 'wb') {|f| f.write text_plain}
+      File.open(output_file, 'wb') {|f| f.write text}
 
       return true
     end
