27ee140e6129490a2bae295e2ac9ed54f47b4974
topac <topac@hackingteam.com>
2013-05-03 12:29:14 +0200

    spec #create_entity_handle

--- tests/rspec/lib/rcs-intelligence/accounts_spec.rb
+++ tests/rspec/lib/rcs-intelligence/accounts_spec.rb
@@ -12,13 +12,61 @@ module Intelligence
       connect_mongoid
       empty_test_db
       Entity.any_instance.stub(:alert_new_entity).and_return nil
+      EntityHandle.any_instance.stub(:check_intelligence_license).and_return true
     end
 
     after { empty_test_db }
 
+    let(:operation) { Item.create!(name: 'test-operation', _kind: 'operation', path: [], stat: ::Stat.new) }
+    let(:target) { Item.create!(name: 'test-target', _kind: 'target', path: [operation._id], stat: ::Stat.new) }
+    let(:agent) { Item.create!(name: 'test-agent', _kind: 'agent', path: target.path+[target._id], stat: ::Stat.new) }
+    let(:target_entity) { Entity.any_in({path: [target._id]}).first }
+
+    # TODO
+    # def create_addressbook_evidence data
+    #   Evidence.collection_class(target._id).create!(da: Time.now.to_i, aid: agent._id, type: 'addressbook', data: data)
+    # end
+
+    def add_handle_to_target_entity attributes
+      entity_handle = EntityHandle.new(attributes)
+      target_entity.handles << entity_handle
+      target_entity.save!
+      entity_handle
+    end
+
+
     it 'should use the Tracer module' do
       described_class.should respond_to :trace
       subject.should respond_to :trace
+    end
+
+
+    describe '#create_entity_handle' do
+      let!(:existing_entity_handle) { add_handle_to_target_entity(level: :automatic, type: :target, name: 'John Snow', handle: 'j.snow') }
+      before { target_entity.handles.should == [existing_entity_handle] }
+
+      context 'name, handle and type match an existing EntityHandle' do
+        it 'do nothing' do
+          described_class.create_entity_handle(target_entity, :automatic, :target, 'j.snow', 'John Snow').should be_nil
+          target_entity.handles.size.should == 1
+        end
+      end
+
+      context 'type, level and handle match an existing EntityHandle' do
+        it 'should update the name attribute of the existing EntityHandle' do
+          new_name = 'Rob Stark'
+          described_class.create_entity_handle(target_entity, :automatic, :target, 'j.snow', new_name)
+          target_entity.handles.size.should == 1
+          target_entity.handles.first.name.should == new_name
+        end
+      end
+
+      context 'type, level, and handle are not found in any existing EntityHandle' do
+        it 'should create a new EntityHandle' do
+          described_class.create_entity_handle(target_entity, :automatic, :target, 'a.stark', 'Arya Stark')
+          target_entity.handles.size.should == 2
+        end
+      end
     end
 
 
