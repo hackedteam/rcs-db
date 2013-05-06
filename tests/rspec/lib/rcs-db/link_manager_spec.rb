7f2ad5e829648aa67b57452c16c9a93478fcc00d
alor <alberto.ornaghi@gmail.com>
2013-05-06 10:54:31 +0200

    testing link manager

--- tests/rspec/lib/rcs-db/link_manager_spec.rb
+++ tests/rspec/lib/rcs-db/link_manager_spec.rb
@@ -13,6 +13,23 @@ module DB
       turn_off_tracer
     end
 
+    context 'given two entities' do
+      before do
+        Entity.any_instance.stub(:alert_new_entity).and_return nil
+        Entity.any_instance.stub(:push_new_entity).and_return nil
+        RCS::DB::LinkManager.any_instance.stub(:alert_new_link).and_return nil
+        RCS::DB::LinkManager.any_instance.stub(:push_modify_entity).and_return nil
+
+        @operation = Item.create!(name: 'test-operation', _kind: 'operation', path: [], stat: ::Stat.new)
+        @entity1 = Entity.create!(name: 'entity1', type: :target, path: [@operation._id], level: :automatic)
+        @entity2 = Entity.create!(name: 'entity2', type: :target, path: [@operation._id], level: :automatic)
+      end
+
+      it 'should not link an entity on itself' do
+        lambda {LinkManager.instance.add_link(from: @entity1, to: @entity1)}.should raise_error
+      end
+    end
+
   end
 
 end
