d08cb67f1715f5f6ffbd57c716630184f04754c1
topac <topac@hackingteam.com>
2013-05-27 11:43:33 +0200

    rspec matcher #be_in_push_queue

--- tests/rspec/spec_matchers.rb
+++ tests/rspec/spec_matchers.rb
@@ -18,3 +18,40 @@ RSpec::Matchers.define :binary_match do |regexp|
     string  =~ regexp
   end
 end
+
+# Check if a mongoid document has been added to the PushQueue
+#
+# @examples:
+#   expect(my_target).to be_in_push_queue
+#   expect(my_target).to be_in_push_queue.with_action(:create)
+#   expect(my_entity).to be_in_push_queue.with_action(:create).exactly 3.times
+#   expect(my_entity).to be_in_push_queue.exactly 7.times
+RSpec::Matchers.define :be_in_push_queue do
+  chain :with_action do |action_name|
+    @action_name = action_name
+  end
+
+  chain :exactly do |count|
+    @count = count.respond_to?(:max) ? count.max+1 : count
+  end
+
+  match do |object|
+    # Only mondoid documents can be added to the PushQueue
+    return false unless object.respond_to? :_id
+
+    # Optional: Filter for the object type
+    filter = {}
+    filter['type'] = 'entity' if object.kind_of? Entity
+
+    filter['message.id'] = object.id
+    filter['message.action'] = "#{@action_name}" if @action_name
+
+    criteria = PushQueue.where(filter)
+
+    if @count
+      criteria.count == @count
+    else
+      criteria.exists?
+    end
+  end
+end
