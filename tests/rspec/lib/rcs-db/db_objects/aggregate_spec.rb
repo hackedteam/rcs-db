29387744c668ce84a605866d4cd4e2ecc5d0335c
topac <topac@hackingteam.com>
2013-07-01 10:02:50 +0200

    fix failing spec, aggregates of type :call do not exists anymore

--- tests/rspec/lib/rcs-db/db_objects/aggregate_spec.rb
+++ tests/rspec/lib/rcs-db/db_objects/aggregate_spec.rb
@@ -108,7 +108,7 @@ describe Aggregate do
       aggregate_class.create!(day: Time.now.strftime('%Y%m%d'), type: :sms, aid: 'agent_id', count: 3, data: {peer: 'test3', versus: :in})
       aggregate_class.create!(day: Time.now.strftime('%Y%m%d'), type: :skype, aid: 'agent_id', count: 1, data: {peer: 'test.ardo', versus: :in})
       aggregate_class.create!(day: Time.now.strftime('%Y%m%d'), type: :skype, aid: 'agent_id', count: 2, data: {peer: 'test.one', versus: :in})
-      aggregate_class.create!(day: Time.now.strftime('%Y%m%d'), type: :call, aid: 'agent_id', count: 3, data: {peer: 'test.ardissimo', versus: :in})
+      aggregate_class.create!(day: Time.now.strftime('%Y%m%d'), type: :phone, aid: 'agent_id', count: 3, data: {peer: 'test.ardissimo', versus: :in})
     end
 
     it 'should be able to rebuild summary' do
@@ -128,7 +128,7 @@ describe Aggregate do
 
       most_contacted.size.should be 3
 
-      most_contacted.should include([{peer: "test.ardissimo", type: :call, count: 3, size: 0, percent: 100.0}])
+      most_contacted.should include([{peer: "test.ardissimo", type: :phone, count: 3, size: 0, percent: 100.0}])
       most_contacted.should include([{peer: "test3", type: :sms, count: 3, size: 0, percent: 50.0}])
       most_contacted.should include([{peer: "test.one", type: :skype, count: 2, size: 0, percent: 66.0}])
     end
@@ -144,7 +144,7 @@ describe Aggregate do
       sms = most_contacted[2]
 
       call.size.should be 1
-      call.should include({peer: "test.ardissimo", type: :call, count: 3, size: 0, percent: 100.0})
+      call.should include({peer: "test.ardissimo", type: :phone, count: 3, size: 0, percent: 100.0})
 
       sms.size.should be 3
       sms.should include({:peer=>"test3", :type=>:sms, :count=>3, :size=>0, :percent=>50.0})
