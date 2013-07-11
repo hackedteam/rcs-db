6f49dd310e4aa6ca5bd8e4378b087c4db50d090b
topac <topac@hackingteam.com>
2013-07-11 16:59:03 +0200

    add indexes

--- lib/rcs-db/db_objects/session.rb
+++ lib/rcs-db/db_objects/session.rb
@@ -1,8 +1,5 @@
 require 'mongoid'
 
-#module RCS
-#module DB
-
 class Session
   include Mongoid::Document
 
@@ -19,8 +16,7 @@ class Session
   belongs_to :user, :dependent => :nullify, :autosave => true
 
   store_in collection: 'sessions'
+
+  index user: 1
+  index cookie: 1
 end
-
-
-#end # ::DB
-#end # ::RCS
