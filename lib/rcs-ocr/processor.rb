b5dfbf7879a53828ba80bf8fd1305e16c6196e92
alor <alberto.ornaghi@gmail.com>
2013-10-04 14:49:53 +0200

    fix for face detection

--- lib/rcs-ocr/processor.rb
+++ lib/rcs-ocr/processor.rb
@@ -77,6 +77,7 @@ class Processor
         processed = LeadTools.transform(temp, output)
         update_evidence_data_body(ev, output, processed)
         # search for faces in screenshots
+        processed = FaceRecognition.detect(temp)
         update_evidence_data_face(ev, processed) if processed.has_key? :face
       when 'file'
         trace :debug, "Extracting text from: #{File.basename(ev.data['path'])}"
