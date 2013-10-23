f1e1da4d62c8a4a914b60dab0c756385875edbd4
topac <topac@hackingteam.com>
2013-10-23 14:44:21 +0200

    use #require_component

--- lib/rcs-intelligence.rb
+++ lib/rcs-intelligence.rb
@@ -1,22 +1,3 @@
-# save the execution dir
-# useful during configuration to get filename paths
-$invocation_directory = Dir.pwd
-$invocation_directory = ENV['CWD'] if ENV['CWD']
+require 'rcs-common/path_utils'
 
-# ensure the working dir is correct
-$execution_directory = File.dirname(File.dirname(File.realpath(__FILE__)))
-Dir.chdir $execution_directory
-
-# load the bundler gem (those taken from git and not installed in rubygems)
-#Bundler.setup
-
-# release file are encrypted and stored in a different directory
-if File.directory?(Dir.pwd + '/lib/rcs-intelligence-release')
-  require_relative 'rcs-intelligence-release/intelligence'
-# otherwise we are using development code
-elsif File.directory?(Dir.pwd + '/lib/rcs-intelligence')
-  puts "WARNING: Executing clear text code... (debug only)"
-  require_relative 'rcs-intelligence/intelligence.rb'
-else
-  puts "FATAL: cannot find any rcs-intelligence code!"
-end
+require_component(:intelligence)
--- lib/rcs-worker.rb
+++ lib/rcs-worker.rb
@@ -1,22 +1,3 @@
-# save the execution dir
-# useful during configuration to get filename paths
-$invocation_directory = Dir.pwd
-$invocation_directory = ENV['CWD'] if ENV['CWD']
+require 'rcs-common/path_utils'
 
-# ensure the working dir is correct
-$execution_directory = File.dirname(File.dirname(File.realpath(__FILE__)))
-Dir.chdir $execution_directory
-
-# load the bundler gem (those taken from git and not installed in rubygems)
-#Bundler.setup
-
-# release file are encrypted and stored in a different directory
-if File.directory?(Dir.pwd + '/lib/rcs-worker-release')
-  require_relative 'rcs-worker-release/worker'
-# otherwise we are using development code
-elsif File.directory?(Dir.pwd + '/lib/rcs-worker')
-  puts "WARNING: Executing clear text code... (debug only)"
-  require_relative 'rcs-worker/worker.rb'
-else
-  puts "FATAL: cannot find any rcs-worker code!"
-end
+require_component(:worker)
