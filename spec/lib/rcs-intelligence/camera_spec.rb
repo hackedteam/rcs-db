6b542c2a5875d6368aff34fd5b77bc7100520a66
topac <topac@hackingteam.com>
2013-11-13 17:11:12 +0100

    fix failing spec

--- spec/lib/rcs-intelligence/camera_spec.rb
+++ spec/lib/rcs-intelligence/camera_spec.rb
@@ -3,52 +3,59 @@ require_db 'db_layer'
 require_db 'grid'
 require_intelligence 'camera'
 
-module RCS
-module Intelligence
+describe RCS::Intelligence::Camera do
 
-  describe Camera do
+  silence_alerts
 
-    silence_alerts
+  let(:operation) { factory_create(:operation) }
 
-    let(:operation) { Item.create!(name: 'test-operation', _kind: 'operation', path: [], stat: ::Stat.new) }
-    let(:target) { Item.create!(name: 'test-target', _kind: 'target', path: [operation._id], stat: ::Stat.new) }
-    let(:agent) { Item.create!(name: 'test-agent', _kind: 'agent', path: target.path+[target._id], stat: ::Stat.new) }
-    let(:entity_with_photos) { Entity.new(photos: ["filename"]) }
-    let(:empty_evidence) { Evidence.dynamic_new('testtarget') }
-    let :camera_evidence do
-      id = RCS::DB::GridFS.put("photo_binary_data", {filename: 'photo_filename'}, target._id.to_s)
-      data = {'_grid' => id, '_grid_size' => 6}
-      Evidence.target(target._id).create!(da: Time.now.to_i, aid: agent._id, type: 'camera', data: data)
-    end
-    # This entity is automatically created when an Item of kind TARGET is saved
-    let(:entity_without_photos) { Entity.any_in({path: [target._id]}).first }
+  let(:target) { factory_create(:target, operation: operation) }
 
-    it 'should use the Tracer module' do
-      described_class.should respond_to :trace
-      subject.should respond_to :trace
-    end
+  let(:agent) { factory_create(:agent, target: target) }
 
-    context '#save_first_camera' do
-      context 'when a photo has been associated to the entity' do
-        it 'should return without adding the new photo' do
-          entity_with_photos.should_not_receive :add_photo
-          described_class.save_picture entity_with_photos, empty_evidence
-        end
+  let(:empty_evidence) { factory_create(:chat_evidence, target: target, agent: agent) }
+
+  let(:camera_evidence) { factory_create(:screenshot_evidence, target: target, agent: agent, type: 'camera', data: {'face' => true}) }
+
+  it 'should use the Tracer module' do
+    described_class.should respond_to :trace
+    subject.should respond_to :trace
+  end
+
+  context '#save_first_camera' do
+
+    let(:entity_with_photos) { factory_create(:target_entity, target: target, photos: ["filename"]) }
+
+    context 'when the camera evidence does not contains a photo with a face' do
+      before do
+        camera_evidence.data['fate'] = false
+        camera_evidence.save!
       end
 
-      context 'when the entity has no photos' do
-        before do
-          camera_evidence
-          entity_without_photos.photos.should be_empty
-        end
+      it 'does nothing' do
+        expect(described_class.save_picture(entity_with_photos, empty_evidence)).to be_nil
+      end
+    end
 
-        it 'should save the new photo' do
-          described_class.save_picture entity_without_photos, camera_evidence
-          entity_without_photos.photos.should_not be_empty
-        end
+    context 'when a photo has been associated to the entity' do
+      it 'should return without adding the new photo' do
+        entity_with_photos.should_not_receive :add_photo
+        described_class.save_picture entity_with_photos, empty_evidence
+      end
+    end
+
+    context 'when the entity has no photos' do
+
+      let(:entity_without_photos) { factory_create(:target_entity, target: target) }
+
+      before do
+        expect(entity_without_photos.photos).to be_empty
+      end
+
+      it 'should save the new photo' do
+        described_class.save_picture(entity_without_photos, camera_evidence)
+        expect(entity_without_photos.photos).not_to be_empty
       end
     end
   end
-
-end
 end
