fc8b997b6082ca1051ffd8216493585be66eae9e
alor <alberto.ornaghi@gmail.com>
2013-11-25 08:44:46 +0100

    remove driver for upgrade of osx

--- lib/rcs-db/db_objects/item.rb
+++ lib/rcs-db/db_objects/item.rb
@@ -398,7 +398,6 @@ class Item
         #add_upgrade('driver64', File.join(build.tmpdir, 'driver64'))
       when 'osx'
         add_upgrade('inputmanager', File.join(build.tmpdir, 'inputmanager'))
-        add_upgrade('driver', File.join(build.tmpdir, 'driver'))
       when 'linux'
         add_upgrade('core32', File.join(build.tmpdir, 'core32'))
         add_upgrade('core64', File.join(build.tmpdir, 'core64'))
