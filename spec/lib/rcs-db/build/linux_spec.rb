114fc3a02e2628c39df87bf521d50fcda1bcd6e7
topac <topac@hackingteam.com>
2013-12-17 15:29:55 +0100

    linux build spec

--- spec/lib/rcs-db/build/linux_spec.rb
+++ spec/lib/rcs-db/build/linux_spec.rb
@@ -9,15 +9,17 @@ require_db 'core'
 module RCS::DB
   describe BuildLinux do
     enable_license
-    stub_temp_folder
 
     let(:local_cores_path) { File.expand_path('../../../../../cores', __FILE__) }
 
     let(:remote_cores_path) { "/Volumes/SHARE/RELEASE/SVILUPPO/cores galileo" }
 
+    let(:melt_file) { 'curl_7.32.0-1ubuntu1_i386.deb' }
+
     before(:all) do
       do_not_empty_test_db
 
+      FileUtils.cp(fixtures_path(melt_file), Config.instance.temp)
       FileUtils.cp("#{remote_cores_path}/linux.zip", "#{local_cores_path}/")
 
       @signature = ::Signature.create!(scope: 'agent', value: 'A'*32)
@@ -33,15 +35,15 @@ module RCS::DB
     end
 
     before do
+      subject.stub(:archive_mode?).and_return false
+      RCS::DB::Build.any_instance.stub(:license_magic).and_return 'WmarkerW'
+
       unless core_loaded?
-        subject.stub(:archive_mode?).and_return false
-        RCS::DB::Build.any_instance.stub(:license_magic).and_return 'WmarkerW'
         RCS::DB::Core.load_core ('./cores/linux.zip')
       end
     end
 
     describe 'linux builder' do
-
       it 'should create the silent installer' do
         params = {
           'factory' => {'_id' => @factory.id},
@@ -52,6 +54,19 @@ module RCS::DB
         subject.create(params)
 
         # # build successful
+        expect(File.size(subject.path(subject.outputs.first))).not_to eql(0)
+      end
+
+      it 'should create the melted installer' do
+        params = {
+          'factory' => {'_id' => @factory.id},
+          'binary'  => {'demo' => false},
+          'melt'    => {'input' => melt_file}
+        }
+
+        subject.create(params)
+
+        # build successful
         expect(File.size(subject.path(subject.outputs.first))).not_to eql(0)
       end
     end
