42788bd255c78bb08c3bc22f54588415c0b942b9
topac <topac@hackingteam.com>
2013-12-20 10:23:28 +0100

    sample

---
+++ spec/lib/rcs-db/build/android_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'fileutils'
+
+require_db 'db_layer'
+require_db 'grid'
+require_db 'build'
+require_db 'core'
+
+require_relative 'shared'
+
+module RCS::DB
+  describe BuildAndroid do
+
+    shared_spec_for(:android)
+
+    before(:all) do
+      @signature = ::Signature.create!(scope: 'agent', value: 'A'*32)
+
+      @factory = Item.create!(name: 'testfactory', _kind: :factory, path: [], stat: ::Stat.new, good: true).tap do |f|
+        f.update_attributes logkey: 'L'*32, confkey: 'C'*32, ident: 'RCS_000000test', seed: '88888888.333'
+        f.configs << Configuration.new({config: 'test_config'})
+      end
+    end
+
+    describe 'Android builder' do
+      it 'should create the silent installer' do
+        params = {
+          'factory' => {'_id' => @factory.id},
+          'binary'  => {'demo' => false},
+          'melt'    => {},
+          'package' => {}
+        }
+
+        subject.create(params)
+
+        expect(File.size(subject.path(subject.outputs.first))).not_to eql(0)
+      end
+    end
+  end
+end
