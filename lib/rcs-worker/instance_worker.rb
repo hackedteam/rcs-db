ac633865ceb591569b8a4005f86342796995af33
alor <alberto.ornaghi@gmail.com>
2014-03-11 17:10:17 +0100

    catch the nomemory error and die hard

--- lib/rcs-worker/instance_worker.rb
+++ lib/rcs-worker/instance_worker.rb
@@ -156,6 +156,13 @@ module RCS
         retry
       rescue MissingAgentError => ex
         raise(ex)
+      rescue ThreadError, NoMemoryError => error
+        msgs = ["[#{error.class}] #{error.message}."]
+        msgs << "There are #{Thread.list.size} active threads. EventMachine threadpool_size is #{EM.threadpool_size}."
+        msgs.concat(error.backtrace) if error.backtrace.respond_to?(:concat)
+
+        trace(:fatal, msgs.join("\n"))
+        exit!(1) # Die hard (will be restarted by windows service manager)
       rescue Exception => e
         trace :fatal, "[#{@agent_uid}] Unrecoverable error processing evidence #{raw_id}: #{e.class} #{e.message}"
         trace :fatal, "[#{@agent_uid}] EXCEPTION: " + e.backtrace.join("\n")
