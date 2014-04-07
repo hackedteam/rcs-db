713505aa75055657396041d7b89666f45e739815
alor <alberto.ornaghi@gmail.com>
2014-04-07 11:20:05 +0200

    cosmetics

--- lib/rcs-db/rest/agent.rb
+++ lib/rcs-db/rest/agent.rb
@@ -363,8 +363,7 @@ class AgentController < RESTController
     return not_found("Factory not found: #{@params['ident']}") if factory.nil?
 
     # the status of the factory must be open otherwise no instance can be cloned from it
-    return not_found("Factory is marked as compromised found: #{@params['ident']}") unless factory.good
-
+    return not_found("Factory marked as compromised found: #{@params['ident']}") unless factory.good
 
     trace :info, "Creating new instance for #{factory[:ident]} (#{factory[:counter]})"
 
